61563e32fdaf8dcf6d65ca0c8ebc8fdc10fc1e12
jvoliveira <jvoliveira@lliege.com.br>
2023-09-01 11:48:26 -0300

    Ajustes e novos testes

--- python-docker/app.py
+++ python-docker/app.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
@@ -12,10 +12,37 @@ def login():
         print("Error:", e)
         raise e
     
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
     try:
-        return render_template('/auth/cadastro.html')
+        if request.method == 'POST':
+            # Get user data from the form
+            nome = request.form['nome']
+            email = request.form['email']
+            senha = request.form['senha']
+
+            # Connect to the MySQL database
+            mydb = mysql.connector.connect(
+                host="mysqldb",
+                user="root",
+                password="p@ssw0rd1",
+                database="tcc"
+            )
+            cursor = mydb.cursor()
+
+            # Insert user data into the usuarios table
+            insert_query = "INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)"
+            user_data = (nome, email, senha)
+            cursor.execute(insert_query, user_data)
+            mydb.commit()
+
+            cursor.close()
+            mydb.close()
+
+            return redirect(url_for('login'))  # Redirect to the login page after successful registration
+
+        return render_template('/auth/cadastro.html')  # Render the registration form for GET requests
+
     except Exception as e:
         print("Error:", e)
         raise e
@@ -36,48 +63,50 @@ def cadastro_padrao():
         print("Error:", e)
         raise e
 
-@app.route('/widgets')
-def get_widgets():
+# @app.route('/users')
+# def get_widgets():
+#     mydb = mysql.connector.connect(
+#         host="mysqldb",
+#         user="root",
+#         password="p@ssw0rd1",
+#         database="tcc"
+#     )
+#     cursor = mydb.cursor()
+
+
+#     cursor.execute("SELECT * FROM widgets")
+
+#     row_headers=[x[0] for x in cursor.description] #this will extract row headers
+
+#     results = cursor.fetchall()
+#     json_data=[]
+#     for result in results:
+#         json_data.append(dict(zip(row_headers,result)))
+
+#     cursor.close()
+
+#     return json.dumps(json_data)
+
+@app.route('/initdb')
+def db_init():
+   
+    # Após criar o banco de dados, você precisa se conectar novamente para usá-lo
     mydb = mysql.connector.connect(
         host="mysqldb",
         user="root",
         password="p@ssw0rd1",
-        database="inventory"
+        database="tcc"
     )
     cursor = mydb.cursor()
 
-
-    cursor.execute("SELECT * FROM widgets")
-
-    row_headers=[x[0] for x in cursor.description] #this will extract row headers
-
-    results = cursor.fetchall()
-    json_data=[]
-    for result in results:
-        json_data.append(dict(zip(row_headers,result)))
-
+    cursor.execute("CREATE TABLE usuarios (  id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, senha VARCHAR(255) NOT NULL)")
+    
+    cursor.execute("CREATE TABLE registros (  id INT AUTO_INCREMENT PRIMARY KEY, arquivo_padrao VARCHAR(255) NOT NULL, coordenada_x DECIMAL(10, 4) NOT NULL, coordenada_y DECIMAL(10, 4) NOT NULL")
+    
     cursor.close()
-
-    return json.dumps(json_data)
-
-@app.route('/initdb')
-def db_init():
-    mydb = mysql.connector.connect(
-        host="mysqldb",
-        user="root",
-        password="p@ssw0rd1"
-    )
-    cursor = mydb.cursor()
-
-    cursor.execute("DROP DATABASE IF EXISTS inventory")
-    cursor.execute("CREATE DATABASE inventory")
-    cursor.execute("USE inventory")
-
-    cursor.execute("DROP TABLE IF EXISTS widgets")
-    cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
-    cursor.close()
+    mydb.close()
 
     return 'init database'
 
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0')
+    app.run(host='0.0.0.0')
