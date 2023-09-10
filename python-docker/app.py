c9174df9d4429ed31c965546d136f66a00d009f6
joaovictor1212 <joaovictor.1212@hotmail.comom>
2023-09-09 23:11:45 -0300

    Ajustes estilizacao dos arquivos

--- python-docker/app.py
+++ python-docker/app.py
@@ -2,7 +2,7 @@ import mysql.connector
 import json
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 
 
 @app.route('/')
