94f5251c045d63eb3581e9e7a82e1ddcf3ab42ea
joaovictor1212 <joaovictor.1212@hotmail.comom>
2023-10-23 20:34:58 -0300

    Inicio das verificacoes

--- CoppeliaSimEdu/VRep-PYTHON/principal /main.py
+++ CoppeliaSimEdu/VRep-PYTHON/principal /main.py
@@ -18,55 +18,70 @@ img_rgb = cv.imread("/home/adduser/ADS/TCC/CoppeliaSimEdu/images/camera.png")
 
 
 # Fazendo uma solicitação GET para o URL
-respostas = requests.get('http://localhost:8000/api/registros')
 
 
+class ValidationImages:
 
-# Verificando se a solicitação foi bem-sucedida (código de status 200)
-if respostas.status_code == 200:
-    # Obtendo os dados da resposta como um dicionário Python
-    dados = respostas.json()
+    @staticmethod
+    def get_images_in_database():
+        respostas = requests.get('http://localhost:8000/api/registros')
+        # Verificando se a solicitação foi bem-sucedida (código de status 200)
+        if respostas.status_code == 200:
+            # Obtendo os dados da resposta como um dicionário Python
+            dados = respostas.json()
 
-    for dado in dados:
-        # with open('./teste.txt', 'w') as arquivo:
-        #  # Escrevendo a string no arquivo
-        #     arquivo.write(dado.get('arquivo'))
-        arquivo_base64 = dado.get('arquivo')
-        imagem_bytes = base64.b64decode(arquivo_base64)
-        # # Converter bytes para uma matriz NumPy
-        imagem_np = np.frombuffer(imagem_bytes, np.uint8)
+            for dado in dados:
+                # with open('./teste.txt', 'w') as arquivo:
+                #  # Escrevendo a string no arquivo
+                #     arquivo.write(dado.get('arquivo'))
+                arquivo_base64 = dado.get('arquivo')
+                imagem_bytes = base64.b64decode(arquivo_base64)
+                # # Converter bytes para uma matriz NumPy
+                imagem_np = np.frombuffer(imagem_bytes, np.uint8)
 
-        # # Decodificar a matriz para uma imagem usando OpenCV
-       
-        # Ou cv2.IMREAD_COLOR para uma imagem colorida
-        # imagem = cv.cvtColor(imagem_np, cv.COLOR_GRAY2BGR)  
-        
-        # Verificar o imdecode ou o cvtColor
-        imagem = cv.imdecode(imagem_np, cv.IMREAD_COLOR) 
-        
-        # Exibir a imagem em uma janela usando OpenCV
-        cv.imshow('Imagem TESTE', imagem)
+                # # Decodificar a matriz para uma imagem usando OpenCV
+            
+                # Ou cv2.IMREAD_COLOR para uma imagem colorida
+                # imagem = cv.cvtColor(imagem_np, cv.COLOR_GRAY2BGR)  
+                
+                # Verificar o imdecode ou o cvtColor
+                imagem = cv.imdecode(imagem_np, cv.IMREAD_COLOR) 
+                
+                # Exibir a imagem em uma janela usando OpenCV
+                cv.imshow('Imagem TESTE', imagem)
 
-        # Esperar até que uma tecla seja pressionada e fechar a janela
-        cv.waitKey(0)
-        cv.destroyAllWindows()
-else:
-    # Se a solicitação não for bem-sucedida, imprima o código de status
-    print('Falha na solicitação. Código de status:', respostas.status_code, respostas.text)
+                # Esperar até que uma tecla seja pressionada e fechar a janela
+                cv.waitKey(0)
+                cv.destroyAllWindows()
+            return imagem
+        else:
+            # Se a solicitação não for bem-sucedida, imprima o código de status
+            print('Falha na solicitação. Código de status:', respostas.status_code, respostas.text)
+
+    @staticmethod
+    def get_image_in_Vrep():
+        assert img_rgb is not None, "file could not be read, check with os.path.exists()"
+        img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+        template = cv.imread('../patterns/quadrado-vermelho.png', cv.IMREAD_GRAYSCALE)
+        assert template is not None, "file could not be read, check with os.path.exists()"
+        w, h = template.shape[::-1]
+        res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+        threshold = 0.8
+        loc = np.where(res >= threshold)
+        for pt in zip(*loc[::-1]):
+            cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+        cv.imwrite('res.png', img_rgb)
 
 
+    @staticmethod
+    def mapping_imagens_with_template():
+        pattern = ValidationImages.get_images_in_database()
+        image_test = ValidationImages.get_image_in_Vrep()
 
-assert img_rgb is not None, "file could not be read, check with os.path.exists()"
-img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-template = cv.imread('../patterns/quadrado-vermelho.png', cv.IMREAD_GRAYSCALE)
-assert template is not None, "file could not be read, check with os.path.exists()"
-w, h = template.shape[::-1]
-res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where(res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-cv.imwrite('res.png', img_rgb)
+        if pattern and image_test:
+            cv.matchTemplate(image_test, pattern, cv.TM_CCOEFF_NORMED)
+
+ValidationImages.mapping_imagens_with_template()
 
 # scara.moveJ(0, 90)
 # scara.moveJ(0, 0)
