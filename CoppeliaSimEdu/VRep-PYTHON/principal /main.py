118e9707f8da9eda34960827cafbe7c5c5fc2ff6
joaovictor1212 <joaovictor.1212@hotmail.comom>
2023-10-31 00:35:22 -0300

    novos testes do match template

--- CoppeliaSimEdu/VRep-PYTHON/principal /main.py
+++ CoppeliaSimEdu/VRep-PYTHON/principal /main.py
@@ -1,3 +1,4 @@
+import os
 import scara
 import cv2 as cv
 import numpy as np
@@ -59,27 +60,55 @@ class ValidationImages:
             print('Falha na solicitação. Código de status:', respostas.status_code, respostas.text)
 
     @staticmethod
-    def get_image_in_Vrep():
-        assert img_rgb is not None, "file could not be read, check with os.path.exists()"
-        img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-        template = cv.imread('../patterns/quadrado-vermelho.png', cv.IMREAD_GRAYSCALE)
-        assert template is not None, "file could not be read, check with os.path.exists()"
-        w, h = template.shape[::-1]
-        res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+    def get_image_to_verify():
+        assert os.path.exists('/home/adduser/ADS/TCC/CoppeliaSimEdu/images/camera.png'), "Arquivo não encontrado, verifique o caminho."
+        # Lê a imagem RGB
+        img_rgb = cv.imread('/home/adduser/ADS/TCC/CoppeliaSimEdu/images/camera.png')
+        return img_rgb
+
+    # @staticmethod
+    # def mapping_imagens_with_template():
+        # assert img_rgb is not None, "file could not be read, check with os.path.exists()"
+        # img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+        # template = cv.imread('../patterns/quadrado-vermelho.png', cv.IMREAD_GRAYSCALE)
+        # assert template is not None, "file could not be read, check with os.path.exists()"
+        # w, h = template.shape[::-1]
+        # res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+        # threshold = 0.8
+        # loc = np.where(res >= threshold)
+        # for pt in zip(*loc[::-1]):
+        #     cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+        # cv.imwrite('res.png', img_rgb)
+
+    @staticmethod
+    def mapping_imagens_with_template():
+        
+        # Obtém a imagem carregada pelo Vrep
+        img_rgb = ValidationImages.get_image_to_verify()
+        assert img_rgb is not None, "Imagem não pode ser lida, verifique o arquivo."
+        img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY) 
+        # Obtém o padrão (template) do banco de dados
+        template = ValidationImages.get_images_in_database()
+        assert template is not None, "Padrão não pode ser lido, verifique o arquivo."
+        template_gray = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
+        # Obtém as dimensões do template
+        w, h= template_gray.shape[::-1]
+        
+        # Faz o matchTemplate
+        res = cv.matchTemplate(img_gray, template_gray, cv.TM_CCOEFF_NORMED)
+        
         threshold = 0.8
         loc = np.where(res >= threshold)
         for pt in zip(*loc[::-1]):
             cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-        cv.imwrite('res.png', img_rgb)
+        
+        # Salva a imagem com os retângulos desenhados
+        cv.imwrite('/home/adduser/ADS/TCC/CoppeliaSimEdu/images/res.png', img_rgb)
 
 
-    @staticmethod
-    def mapping_imagens_with_template():
-        pattern = ValidationImages.get_images_in_database()
-        image_test = ValidationImages.get_image_in_Vrep()
 
-        if pattern and image_test:
-            cv.matchTemplate(image_test, pattern, cv.TM_CCOEFF_NORMED)
+    
+    
 
 ValidationImages.mapping_imagens_with_template()
 
