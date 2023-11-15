ec7c3e92675eca76abeb46f4bf624ba328abcb1c
joaovictor1212 <joaovictor.1212@hotmail.comom>
2023-11-15 13:11:27 -0300

    Ajuste dos arquivos flask finalizacao CRUD

--- python-docker/app.py
+++ python-docker/app.py
@@ -1,6 +1,7 @@
 import mysql.connector
-from flask import Flask, render_template, request, redirect, url_for, jsonify, json
+from flask import Flask, render_template, request, redirect, url_for, jsonify, json, Response
 import base64
+import os
 
 
 app = Flask(__name__)
@@ -13,7 +14,7 @@ def login():
     except Exception as e:
         print("Error:", e)
         raise e
-    
+
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
     try:
@@ -88,7 +89,7 @@ def registros():
             return redirect(url_for('registros'))
 
         return render_template('/principal/registros.html')
-    
+
     except Exception as e:
         print("Error:", e)
         raise e
@@ -111,7 +112,7 @@ def trazer_registros():
         registros = cursor.fetchall()  # Obtendo todos os registros da consulta
         cursor.close()
         mydb.close()
-        
+
         # Convertendo registros para um formato serializável (por exemplo, lista de dicionários)
         registros_serializaveis = []
         for registro in registros:
@@ -127,26 +128,89 @@ def trazer_registros():
             registros_serializaveis.append(registro_dict)
 
         return jsonify(registros_serializaveis)
+
+
+@app.route('/lista-registros')
+def lista_registros():
+    resposta = trazer_registros()
+
+    # Verificar se a resposta é um objeto de resposta Flask
+    if isinstance(resposta, Response):
+        # Se for, extrair os dados do conteúdo da resposta
+        registros = resposta.get_json() # Use o método adequado para extrair os dados
+        for registro_id in registros:
+            editar_registro(registro_id=registro_id)
+            excluir_registro(registro_id=registro_id)
+    else:
+        # Se não for, assumir que a resposta já contém os dados necessários
+        registros = resposta
+
+    try:
+        return render_template('/principal/lista-registros.html', registros=registros)
+    except Exception as e:
+        print("Error:", e)
+        raise e
+
+
+@app.route('/editar-registro/<int:registro_id>', methods=['GET', 'POST'])
+def editar_registro(registro_id):
+
+    if request.method == 'POST':
+        # Após criar o banco de dados, você precisa se conectar novamente para usá-lo
+        if registro_id:
+            mydb = mysql.connector.connect(
+                host="mysqldb",
+                user="root",
+                password="p@ssw0rd1",
+                database="tcc"
+            )
+            cursor = mydb.cursor()
+
+            # Usando placeholders para evitar injeção de SQL
+            query = "SELECT * FROM registros WHERE id = %s"
+            cursor.execute(query, (registro_id,))
+            data = cursor.fetchall()
+            
+            cursor.close()
+            mydb.close()
+            if data:
+                print("Registro encontrado com sucesso!")
+                return redirect(url_for('lista_registros'))
     
 
-# @app.route('/lista-registros')
-# def lista_registros():
-#     resposta = trazer_registros()
-#     print(resposta)
-    # result = json.dumps(resposta)
-    # if result:
-    #     try:
-    #         return render_template('/principal/lista-registros.html', registros=result)
-    #     except Exception as e:
-    #         print("Error:", e)
-    #         raise e
+
+# Rota para excluir um registro específico
+@app.route('/excluir-registro/<int:registro_id>', methods=['POST'])
+def excluir_registro(registro_id):
+    # Verifica se o método da requisição é POST
+    if request.method == 'POST':
+        # Após criar o banco de dados, você precisa se conectar novamente para usá-lo
+        if registro_id:
+            mydb = mysql.connector.connect(
+                host="mysqldb",
+                user="root",
+                password="p@ssw0rd1",
+                database="tcc"
+            )
+            cursor = mydb.cursor()
+
+            # Usando placeholders para evitar injeção de SQL
+            query = "DELETE FROM registros WHERE id = %s"
+            cursor.execute(query, (registro_id,))
+
+            mydb.commit()
+            cursor.close()
+            mydb.close()
+            print("Registro excluído com sucesso!")
+
+    return redirect(url_for('lista_registros'))
 
 
 
 
 @app.route('/initdb', methods=['GET','POST'])
 def cadastro_usuario():
-   
+
     # Após criar o banco de dados, você precisa se conectar novamente para usá-lo
     mydb = mysql.connector.connect(
         host="mysqldb",
@@ -157,8 +221,8 @@ def cadastro_usuario():
     cursor = mydb.cursor()
 
     cursor.execute("CREATE TABLE IF NOT EXISTS usuarios (  id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, senha VARCHAR(255) NOT NULL);")
-    
-    
+
+
     cursor.close()
     mydb.close()
 
@@ -167,7 +231,7 @@ def cadastro_usuario():
 
 @app.route('/initdb', methods=['GET','POST'])
 def cadastro_registros():
-   
+
     # Após criar o banco de dados, você precisa se conectar novamente para usá-lo
     mydb = mysql.connector.connect(
         host="mysqldb",
@@ -177,9 +241,9 @@ def cadastro_registros():
     )
     cursor = mydb.cursor()
 
-   
+
     cursor.execute("CREATE TABLE IF NOT EXISTS registros (  id INT AUTO_INCREMENT PRIMARY KEY, arquivo LONGBLOB, coordenada_x DECIMAL(10, 4) NOT NULL, coordenada_y DECIMAL(10, 4) NOT NULL);")
-    
+
     cursor.close()
     mydb.close()
 
