e7a5fb267810ea459d744949ba1b93376f8c9fbc
joaovictor1212 <joaovictor.1212@hotmail.com>
2023-11-26 18:47:50 -0300

    Ajustes para template

--- CoppeliaSimEdu/VRep-PYTHON/principal /main.py
+++ CoppeliaSimEdu/VRep-PYTHON/principal /main.py
@@ -7,6 +7,7 @@ import requests
 import base64
 import io
 import binascii
+import imutils
 
 # Inicia Simulação
 scara.start()
@@ -40,100 +41,91 @@ class ValidationImages:
                 # # Converter bytes para uma matriz NumPy
                 imagem_np = np.frombuffer(imagem_bytes, np.uint8)
                 # # Decodificar a matriz para uma imagem usando OpenCV
-            
+
                 # # Ou cv2.IMREAD_COLOR para uma imagem colorida
                 # imagem = cv.cvtColor(imagem_np, cv.COLOR_GRAY2BGR)  
-                
+
                 # Verificar o imdecode ou o cvtColor
-                imagem = cv.imdecode(imagem_np, cv.IMREAD_COLOR) 
-                
+                imagem = cv.imdecode(imagem_np, cv.IMREAD_COLOR)
+
                 # Exibir a imagem em uma janela usando OpenCV
                 cv.imshow('Imagem TESTE', imagem)
                 # Esperar até que uma tecla seja pressionada e fechar a janela
                 cv.waitKey(0)
                 cv.destroyAllWindows()
-                
+
                 dict_image = {
-                    'nome' : count,
+                    'nome': count,
                     'imagem': imagem,
                     'x': x,
                     'y': y,
                 }
                 count += 1
                 imagens_coordenadas.append(dict_image)
-                
-            
-                
+
             return imagens_coordenadas
         else:
             # Se a solicitação não for bem-sucedida, imprima o código de status
             print('Falha na solicitação. Código de status:', respostas.status_code, respostas.text)
 
-    
-
-
-
     @staticmethod
     def mapping_imagens_with_template():
+        import pdb;pdb.set_trace()
         global img_rgb
         assert img_rgb is not None, "file could not be read, check with os.path.exists()"
-        img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-        cv.normalize(img_gray, img_gray, 0, 255, cv.NORM_MINMAX)
-        
-        data = ValidationImages.get_images_in_database()
-        
-        # import pdb;pdb.set_trace()
-        if data:
-            for template_database in data:
-                if template_database:
-                    template = cv.cvtColor(template_database.get('imagem'), cv.COLOR_BGR2GRAY)
-                    assert template is not None, "file could not be read, check with os.path.exists()"
-                    altura, largura = template.shape
-                    cv.normalize(template, template, 0, 255, cv.NORM_MINMAX)
+        alturaVrep, larguraVrep, channelsVrep = img_rgb.shape
+        img_rgb = imutils.resize(img_rgb, width=round(larguraVrep * 0.5))
 
-                    
-                    res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
-                    
-                    threshold = 0.8
-                    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-                    nome = template_database.get('nome')
-                    if max_val >= threshold:
-                        ValidationImages.create_result_image(nome=nome, max_loc=max_loc, img_rgb=img_rgb, largura=largura, altura=altura)
-                        ValidationImages.move_in_vrep(coordenada_x=template_database.get('x'), coordenada_y=template_database.get('y'))
-						
-                    else:
-                        print("Padrão %s não conseguiu realizar o macthTemplate." %nome)
-                        continue    
-                    
-    
+        # Pegando os padroes no banco
+        padroes = None
+        padroes = ValidationImages.get_images_in_database()
+
+        if padroes:
+            for padrao in padroes:
+                nome = "IMAGEM-" + str(padrao.get('nome'))
+                template = cv.cvtColor(padrao.get('imagem'), cv.COLOR_BGR2RGB)
+                assert template is not None, "file could not be read, check with os.path.exists()"
+                alturaTemplate, larguraTemplate, channelsTemplate = template.shape
+                template = imutils.resize(template, width=round(larguraTemplate * 0.5))
+
+                res = cv.matchTemplate(img_rgb, template, cv.TM_CCOEFF_NORMED, )
+
+                minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(res)
+
+                print(minVal, maxVal)
+                if maxVal > 0.8:
+                    ValidationImages.create_result_image(nome=nome, maxLoc=maxLoc, img_rgb=img_rgb,
+                                                         largura=larguraTemplate, altura=alturaTemplate)
+                    ValidationImages.move_in_vrep(coordenada_x=padrao.get('x'), coordenada_y=padrao.get('y'))
+                else:
+                    print("Padrão %s não conseguiu realizar o macthTemplate." % nome)
+                    continue
+
+
+    @staticmethod
+    def create_result_image(maxLoc, img_rgb, altura, largura, nome):
+        (startX, startY) = maxLoc
+        endX = startX + largura
+        endY = startY + altura
+
+        # Desenhando retângulo de detecção
+        color = (255, 0, 0)
+        cv.rectangle(img_rgb, (startX, startY), (endX, endY), color, 3)
+        cv.imwrite('/home/joao/ADS/TCC/CoppeliaSimEdu/images/result.png', img_rgb)
+        print("Padrão %s conseguiu realizar o matchTemplate." % nome)
+
+
+
     @staticmethod
     def move_in_vrep(coordenada_x, coordenada_y):
-        import pdb;pdb.set_trace()
         if coordenada_x and coordenada_y:
             x = float(coordenada_x)
             y = float(coordenada_y)
             scara.moveJ(x, y)
-            scara.moveJ((x - x), (y - y)) 
-            scara.moveJ((x - x), (0 - y)) 
             scara.moveJ((x - x), (y - y))
-        scara.stop()
-    
-    
-    @staticmethod
-    def create_result_image(max_loc, img_rgb, altura, largura, nome):
-        top_left = max_loc
-        # Calculate the center of the square.
-        center = (top_left[0] + largura / 2, top_left[1] + altura / 2)
-        # Move the square to the desired location.
-        center = (center[0] + 100, center[1] + 100)
-        # Update the top_left and bottom_right coordinates of the square.
-        top_left = (int(center[0] - largura / 2), int(center[1] - altura / 2))
-        bottom_right = (top_left[0] + largura, top_left[1] + altura)
-        color = (255, 255, 0)
-        cv.rectangle(img_rgb, top_left, bottom_right, color, 2)
-        cv.imwrite('/home/adduser/ADS/TCC/CoppeliaSimEdu/images/result.png', img_rgb)
-        print("Padrão %s conseguiu realizar o matchTemplate." %nome)
-
+            scara.moveJ((x - x), (0 - y))
+            scara.moveJ((x - x), (y - y))
+            scara.stop()
 
 
 
@@ -141,23 +133,3 @@ class ValidationImages:
 ValidationImages.mapping_imagens_with_template()
 
 
-
-
-
-
-
-
-
-# scara.moveJ(0, 90)
-# scara.moveJ(0, 0)
-# scara.moveJ(0, -90)
-# scara.moveJ(0, 0)
-
-# scara.moveJ(90, 0)
-# scara.moveJ(0, 0)
-# scara.moveJ(-90, 0)
-# scara.moveJ(0, 0)
-
-
-# Finaliza Simulação
-# scara.stop()
